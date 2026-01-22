8e6ac0f88ebfdb7a97d67a7e50d8f8f46dd8bbfe
Henrique Santos <thenrique395@gmail.com>
2026-01-21 23:19:28 -0300

    ajustes

--- Infrastructure/Logging/CorrelationIdMiddleware.cs
+++ Infrastructure/Logging/CorrelationIdMiddleware.cs
@@ -2,17 +2,11 @@ using Microsoft.Extensions.Primitives;
 
 namespace InvestindoEmNegocio.Infrastructure.Logging;
 
-public class CorrelationIdMiddleware
+public class CorrelationIdMiddleware(RequestDelegate next, ILogger<CorrelationIdMiddleware> logger)
 {
     public const string HeaderName = "X-Correlation-ID";
-    private readonly RequestDelegate _next;
-    private readonly ILogger<CorrelationIdMiddleware> _logger;
-
-    public CorrelationIdMiddleware(RequestDelegate next, ILogger<CorrelationIdMiddleware> logger)
-    {
-        _next = next;
-        _logger = logger;
-    }
+    private readonly RequestDelegate _next = next;
+    private readonly ILogger<CorrelationIdMiddleware> _logger = logger;
 
     public async Task InvokeAsync(HttpContext context)
     {
